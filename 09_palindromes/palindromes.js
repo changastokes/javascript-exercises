c61f74c518b7e94d1b2d33c42e13bab2eeaae4ce
Changa Stokes <changastokes@gmail.com>
2022-09-14 23:37:33 -0400

    fibonacci finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,31 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let stripPunc = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    //console.log(stripPunc);
+    let finalStripPunc = stripPunc.replace(/ /g,"");
+    //console.log(finalStripPunc);
+    let finalStrippedStr = finalStripPunc.toLowerCase()
+    //console.log(finalStrippedStr);
+    let splitString = finalStrippedStr.split("");
+    console.log(splitString);
+    let reverseArray = splitString.reverse();
+    //console.log(reverseArray);
+    let finalReverseStr = reverseArray.join("");
+    console.log(finalReverseStr);
+
+    if (finalStrippedStr === finalReverseStr) {
+        return true;
+    }else return false;
+
+    /*Cleaner way 
+    const palindromes = function(string) {
+        processedString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+        return (
+            processedString
+            .split("")
+            .reverse()
+            .join("") == processedString
+        );
+    }*/
 
 };
 
